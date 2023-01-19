24a89d67887d0b3d10cf8e2c8fecf73cb478234c
Divyansh Agarwal <divyanshagarwal188@gmail.com>
2023-01-20 01:50:15 +0530

    predict api tested succssfully

--- app.py
+++ app.py
@@ -5,9 +5,22 @@ from flask import Flask, request, app, jsonify, url_for, render_template
 
 app = Flask(__name__)
 
+model = pickle.load(open("regmodel.pkl", "rb"))
+scalar = pickle.load(open("scaler.pkl", "rb"))
+
 @app.route("/")
-def hello():
-    return "Hello World!!"
+def home():
+    return render_template("home.html")
+
+@app.route("/predict_api", methods=["POST"])
+def predict_api():
+    data = request.json["data"]
+    print("data", data)
+    print("reshaped_data", np.array(list(data.values())).reshape(1,-1))
+    scaled_data = scalar.fit_transform(np.array(list(data.values())).reshape(1,-1))
+    output = model.predict(scaled_data)
+    print("output", output[0])
+    return jsonify(output[0])
 
 if __name__ == "__main__":
     try:
@@ -15,4 +28,4 @@ if __name__ == "__main__":
     except Exception as e:
         print("Error Occured!", e)
     else:
-        print("success!")
+        print("success!") 
